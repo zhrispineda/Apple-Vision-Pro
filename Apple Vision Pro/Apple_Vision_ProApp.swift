6dd7a35540d88ea1e29ba9e0251fe40e9eb75c30
Chris Pineda <zhris@zhr.is>
2024-02-09 22:40:07 -0600

    Initial Commit

---
+++ Apple Vision Pro/Apple_Vision_ProApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_Vision_ProApp.swift
+//  Apple Vision Pro
+//
+//  Created by Chris on 2/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Apple_Vision_ProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
